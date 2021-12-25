ebe5bad47aa6b0b49fc887c8359f8587ac2fecf1
Pavlo Kravchuk <kravchuk.pavlo@ukr.net>
2021-12-26 00:18:53 +0100

    Create MoviesPage.jsx

---
+++ src/pages/MoviesPage/MoviesPage.jsx
@@ -0,0 +1,39 @@
+import { useState } from 'react';
+import { toast } from 'react-toastify';
+import styles from './MoviesPage.module.css';
+
+export default function MoviesPage() {
+  const [searchQuery, setSearchQuery] = useState('');
+
+  const handleFormSubmit = event => {
+    event.preventDefault();
+
+    if (searchQuery.trim() !== '') {
+      //   onSubmit(searchQuery);
+      setSearchQuery('');
+    } else toast.error('input field must not be empty');
+  };
+
+  return (
+    <>
+      <h2>сторінка пошуку кінофільмів за ключовим словом</h2>
+      <form onSubmit={handleFormSubmit} className={styles.MoviesPage}>
+        <input
+          className={styles.MoviesPage__input}
+          type="text"
+          autoComplete="off"
+          autoFocus
+          placeholder="search movies"
+          value={searchQuery}
+          onChange={event =>
+            setSearchQuery(event.currentTarget.value.toLowerCase())
+          }
+        />
+
+        <button type="submit" className={styles.MoviesPage__button}>
+          search
+        </button>
+      </form>
+    </>
+  );
+}
