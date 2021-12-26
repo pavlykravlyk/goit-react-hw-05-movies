829a9f3858b36460516946209b374853d5edd418
Pavlo Kravchuk <kravchuk.pavlo@ukr.net>
2021-12-26 23:43:36 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import * as React from 'react';
-import { Routes, Route, Link, NavLink } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Container from './components/Container/Container';
 import PageHeader from './components/PageHeader/PageHeader';
+
 import HomePage from './pages/HomePage/HomePage';
+// const HomePage = React.lazy(() => import('./pages/HomePage/HomePage.jsx'));
 import MoviesPage from './pages/MoviesPage/MoviesPage';
+// const MoviesPage = React.lazy(() =>
+//   import('./pages/MoviesPage/MoviesPage.jsx'),
+// );
 import MovieDetailPage from './pages/MovieDetailsPage/MovieDetailPage';
+// const MovieDetailPage = React.lazy(() =>
+//   import('./pages/MovieDetailsPage/MovieDetailPage.jsx'),
+// );
 
 export default function App() {
   return (
@@ -16,7 +24,7 @@ export default function App() {
         <PageHeader />
         {/* <h1>Welcome to React Router!</h1> */}
         <Routes>
-          <Route path="*" element={<HomePage />}></Route>
+          <Route path="*" element={<HomePage />} />
           <Route path="/movies" element={<MoviesPage />} />
           <Route path="/movies/:movieId/*" element={<MovieDetailPage />} />
         </Routes>
