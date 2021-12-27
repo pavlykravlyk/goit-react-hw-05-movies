73e138ce8ca6988c87938445b301b09a2592321e
Pavlo Kravchuk <kravchuk.pavlo@ukr.net>
2021-12-28 00:42:09 +0100

    Update MoviesPage.jsx

--- src/pages/MoviesPage/MoviesPage.jsx
+++ src/pages/MoviesPage/MoviesPage.jsx
@@ -1,23 +1,46 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import { toast } from 'react-toastify';
-import styles from './MoviesPage.module.css';
+import * as api from '../../services/themoviedb-api';
+import styles from '../HomePage/HomePage.module.css';
 
 export default function MoviesPage() {
   const [searchQuery, setSearchQuery] = useState('');
+  const [movies, setMovies] = useState([]);
+  const [error, setError] = useState('null');
+  const [status, setStatus] = useState('idle');
 
-  const handleFormSubmit = event => {
-    event.preventDefault();
-
-    if (searchQuery.trim() !== '') {
-      //   onSubmit(searchQuery);
-      setSearchQuery('');
-    } else toast.error('input field must not be empty');
-  };
+  async function getMovieByQuery() {
+    try {
+      const response = await api.fetchMovieByQuery(searchQuery);
+      if (response.ok) {
+        const data = await response.json();
+        setMovies(data.results);
+        setStatus('resolved');
+      } else {
+        return Promise.reject(new Error(`Movie ${searchQuery} not found`));
+      }
+    } catch {
+      setError(error);
+      setStatus('rejected');
+      toast.error(error.message);
+    }
+  }
 
   return (
     <section>
-      <h2>сторінка пошуку кінофільмів за ключовим словом</h2>
-      <form onSubmit={handleFormSubmit} className={styles.MoviesPage}>
+      <form
+        onSubmit={event => {
+          event.preventDefault();
+
+          searchQuery.trim() !== ''
+            ? getMovieByQuery(searchQuery)
+            : toast.error('input field must not be empty');
+
+          setSearchQuery('');
+        }}
+        className={styles.MoviesPage}
+      >
         <input
           className={styles.MoviesPage__input}
           type="text"
@@ -29,11 +52,20 @@ export default function MoviesPage() {
             setSearchQuery(event.currentTarget.value.toLowerCase())
           }
         />
-
         <button type="submit" className={styles.MoviesPage__button}>
           search
         </button>
       </form>
+
+      {status === 'resolved' && (
+        <ul className={styles.list}>
+          {movies.map(({ id, title }) => (
+            <li key={id}>
+              <Link to={`/movies/${id}`}>{title}</Link>
+            </li>
+          ))}
+        </ul>
+      )}
     </section>
   );
 }
