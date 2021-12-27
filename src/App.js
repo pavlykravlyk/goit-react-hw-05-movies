d9877c322a9ba615ab6034cd87a775569dfa790e
Pavlo Kravchuk <kravchuk.pavlo@ukr.net>
2021-12-28 00:41:50 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,33 +1,34 @@
-import * as React from 'react';
+import { lazy, Suspense } from 'react';
 import { Routes, Route } from 'react-router-dom';
-import './App.css';
+import styles from './App.module.css';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import Loader from 'react-loader-spinner';
 import Container from './components/Container/Container';
 import PageHeader from './components/PageHeader/PageHeader';
 
-import HomePage from './pages/HomePage/HomePage';
-// const HomePage = React.lazy(() => import('./pages/HomePage/HomePage.jsx'));
-import MoviesPage from './pages/MoviesPage/MoviesPage';
-// const MoviesPage = React.lazy(() =>
-//   import('./pages/MoviesPage/MoviesPage.jsx'),
-// );
-import MovieDetailPage from './pages/MovieDetailsPage/MovieDetailPage';
-// const MovieDetailPage = React.lazy(() =>
-//   import('./pages/MovieDetailsPage/MovieDetailPage.jsx'),
-// );
+const HomePage = lazy(() => import('./pages/HomePage/HomePage.jsx'));
+const MoviesPage = lazy(() => import('./pages/MoviesPage/MoviesPage.jsx'));
+const MovieDetailPage = lazy(() =>
+  import('./pages/MovieDetailsPage/MovieDetailPage.jsx'),
+);
 
 export default function App() {
   return (
-    <div className="App">
+    <div className={styles.App}>
       <Container>
         <PageHeader />
-        {/* <h1>Welcome to React Router!</h1> */}
-        <Routes>
-          <Route path="*" element={<HomePage />} />
-          <Route path="/movies" element={<MoviesPage />} />
-          <Route path="/movies/:movieId/*" element={<MovieDetailPage />} />
-        </Routes>
+        <Suspense
+          fallback={
+            <Loader type="ThreeDots" color="gray" height={80} width={80} />
+          }
+        >
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/movies" element={<MoviesPage />} />
+            <Route path="/movies/:movieId/*" element={<MovieDetailPage />} />
+          </Routes>
+        </Suspense>
       </Container>
 
       <ToastContainer autoClose={3000} />
