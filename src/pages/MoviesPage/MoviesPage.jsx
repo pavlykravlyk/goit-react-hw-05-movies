5e117168a283b1ba5385c5966be538b19f9a28d9
Pavlo Kravchuk <kravchuk.pavlo@ukr.net>
2021-12-28 07:40:16 +0100

    Update MoviesPage.jsx

--- src/pages/MoviesPage/MoviesPage.jsx
+++ src/pages/MoviesPage/MoviesPage.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { toast } from 'react-toastify';
 import * as api from '../../services/themoviedb-api';
