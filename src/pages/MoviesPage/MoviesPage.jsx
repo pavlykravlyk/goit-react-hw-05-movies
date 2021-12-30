c5f54524dad5843b97b641800ee45d8196f8fe24
Pavlo Kravchuk <kravchuk.pavlo@ukr.net>
2021-12-30 01:09:36 +0100

    useSearchParams

--- src/pages/MoviesPage/MoviesPage.jsx
+++ src/pages/MoviesPage/MoviesPage.jsx
@@ -1,51 +1,66 @@
-import { useState, useEffect } from 'react';
-import { Link, useNavigate, useLocation } from 'react-router-dom';
+import { useState, useEffect, useRef } from 'react';
+import {
+  Link,
+  // useNavigate,
+  // useLocation,
+  useSearchParams,
+} from 'react-router-dom';
 import { toast } from 'react-toastify';
 import * as api from '../../services/themoviedb-api';
 import styles from '../HomePage/HomePage.module.css';
+import Loader from 'react-loader-spinner';
 
 export default function MoviesPage() {
-  let navigate = useNavigate();
-  // console.log(navigate);
-  const location = useLocation();
-  // console.log(location.search.slice(1));
+  // const navigate = useNavigate();
+  // const location = useLocation();
 
-  const [searchQuery, setSearchQuery] = useState('');
-  const [movies, setMovies] = useState([]);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const searchQuery = searchParams.get('query');
+  const [query, setQuery] = useState('');
+  const [movies, setMovies] = useState(null);
   const [error, setError] = useState('null');
   const [status, setStatus] = useState('idle');
+
+  useEffect(() => {
+    async function getMovieByQuery() {
+      try {
+        const response = await api.fetchMovieByQuery(searchQuery);
+        if (response.ok) {
+          const data = await response.json();
+          setMovies(data.results);
+          setStatus('resolved');
+        } else {
+          return Promise.reject(new Error(`Movie ${searchQuery} not found`));
+        }
+      } catch {
+        setError(error);
+        setStatus('rejected');
+        toast.error(error.message);
+      }
+    }
+
+    if (searchQuery) {
+      setStatus('pending');
+      getMovieByQuery();
+    }
+
+    // return () => {};
+  }, [searchQuery]);
 
   function handleFormSubmit(event) {
     event.preventDefault();
 
-    if (searchQuery.trim() !== '') {
-      getMovieByQuery(searchQuery);
-      navigate({ search: `query=${searchQuery}` });
-      setSearchQuery('');
+    if (query.trim() !== '') {
+      setSearchParams({ query });
+      setQuery('');
     } else {
       toast.error('input field must not be empty');
     }
   }
 
   function handleInputChange(event) {
-    setSearchQuery(event.currentTarget.value.toLowerCase());
-  }
-
-  async function getMovieByQuery() {
-    try {
-      const response = await api.fetchMovieByQuery(searchQuery);
-      if (response.ok) {
-        const data = await response.json();
-        setMovies(data.results);
-        setStatus('resolved');
-      } else {
-        return Promise.reject(new Error(`Movie ${searchQuery} not found`));
-      }
-    } catch {
-      setError(error);
-      setStatus('rejected');
-      toast.error(error.message);
-    }
+    const inputValue = event.currentTarget.value;
+    setQuery(inputValue.toLowerCase());
   }
 
   return (
@@ -57,13 +72,17 @@ export default function MoviesPage() {
           autoComplete="off"
           autoFocus
           placeholder="search movies"
-          value={searchQuery}
+          value={query}
           onChange={handleInputChange}
         />
         <button type="submit" className={styles.MoviesPage__button}>
           search
         </button>
       </form>
+
+      {status === 'pending' && (
+        <Loader type="ThreeDots" color="gray" height={80} width={80} />
+      )}
 
       {status === 'resolved' && (
         <ul className={styles.list}>
@@ -77,3 +96,9 @@ export default function MoviesPage() {
     </section>
   );
 }
+
+// setSearchParams({ query: event.currentTarget.value.toLowerCase() });
+// navigate({
+//   ...location,
+//   search: `query=${event.currentTarget.value.toLowerCase()}`,
+// });
